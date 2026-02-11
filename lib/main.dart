20b8c0e3b03bd4702088eb02865e060b26d94340
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-02-11 20:52:35 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.blue),
       ),
-      home: ,
+      home: Placeholder(),
     );
   }
 }
